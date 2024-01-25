20f34bc0114bc1fe0751f26424c0ca26a156a5b0
Alexander Petrovichev <apetrovichev@me.com>
2024-01-25 11:56:45 +0300

    Removed unnecessary code

--- Metaperson2.0/AppDelegate.swift
+++ Metaperson2.0/AppDelegate.swift
@@ -12,17 +12,17 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let vc = MainController.instantiateFromStoryboard("Main")
+        let nvc = UINavigationController(rootViewController: vc)
+        
+        window = UIWindow()
+        window?.rootViewController = nvc
+        window?.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
